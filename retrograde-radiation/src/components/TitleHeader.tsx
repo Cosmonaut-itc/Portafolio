6b193711e9266d737e4e4203b8a70becbeacb88e
Felix De Haro <felixddhs@outlook.com>
2024-01-08 13:41:24 -0600

    agregué el primer componente de react y shadcn ui

---
+++ retrograde-radiation/src/components/TitleHeader.tsx
@@ -0,0 +1,12 @@
+export function TitleHeader(){
+    return (
+        <div className='px-10 pt-5'>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Felix De Haro
+            </h1>
+            <h2 className="mt-2 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                Software Engineer
+            </h2>
+        </div>
+    )
+}
